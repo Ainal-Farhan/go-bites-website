668f7c0a5b30a23fdca83d30d296843aa395377f
Mohd Ainal Farhan Mohamad Johari <56626447+Ainal-Farhan@users.noreply.github.com>
2021-04-10 16:39:37 +0800

    Update http-common.js

--- src/http/common/http-common.js
+++ src/http/common/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "https://go-bites-server.herokuapp.com/api",
+    baseURL: "http://pharveish.ml/api",
     // baseURL: "http://localhost:8080/api",
     headers: {
         "Content-Type": "application/json"
